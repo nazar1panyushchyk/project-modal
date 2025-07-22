5e5aef1321953d10529e18ce5277b705543c0211
Daniil10Sh <danik2022odessa@gmail.com>
2025-07-22 19:48:11 +0300

    initialization  project

---
+++ src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Modal.css";
+
+export default class Modal extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Modal window</h1>
+      </>
+    );
+  }
+}
